fe94fc2b55d45df07eadddbca0ee411409b56177
TechnoStudy <ismettechno@gmail.com>
2025-03-17 18:56:38 +0000

    _01_Soru çözümü

--- src/Gun07/_01_Soru.java
+++ src/Gun07/_01_Soru.java
@@ -1,6 +1,15 @@
 package Gun07;
 
-public class _01_Soru {
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class _01_Soru extends BaseDriver {
     //        Senaryo
 //        1- https://www.saucedemo.com/
 //        2- login işlemini yapınız.
@@ -14,4 +23,97 @@ public class _01_Soru {
 //        Item total e eşit olup olmadığını Assert ile test ediniz.
 //
 //        Bu soruda tamamen XPATH kullanılacaktır.
+
+    @Test
+    public void SepetTesti()
+    {
+        driver.get("https://www.saucedemo.com/");
+        MyFunc.Bekle(2);
+
+        WebElement username= driver.findElement(By.xpath("//input[@id='user-name']"));
+        username.sendKeys("standard_user");
+        MyFunc.Bekle(1);
+
+        WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
+        password.sendKeys("secret_sauce");
+        MyFunc.Bekle(1);
+
+        WebElement loginButton= driver.findElement(By.xpath("//input[@id='login-button']"));
+        loginButton.click();
+        MyFunc.Bekle(1);
+
+        //1.Ürün sepete ekleniyor
+        MyFunc.Bekle(2);  //   "//a[@id='item_4_title_link']/div"
+        WebElement product1= driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']"));
+        product1.click();
+        MyFunc.Bekle(2);
+
+        WebElement addToCart= driver.findElement(By.xpath("//button[@id='add-to-cart']"));
+        addToCart.click();
+        MyFunc.Bekle(2);
+
+        WebElement btnBack= driver.findElement(By.xpath("//button[@id='back-to-products']"));
+        btnBack.click();
+        MyFunc.Bekle(2);
+
+        //1.Ürün sepete ekleniyor
+        WebElement product2= driver.findElement(By.xpath("//a[@id='item_1_title_link']/div"));
+        product2.click();
+        MyFunc.Bekle(2);
+
+        WebElement addToCart2= driver.findElement(By.xpath("//button[@id='add-to-cart']"));
+        addToCart2.click();
+        MyFunc.Bekle(2);
+
+        //sepete tıklatılıyor
+        MyFunc.Bekle(2);
+        WebElement sepet= driver.findElement(By.xpath("//a[@class='shopping_cart_link']"));
+        sepet.click();
+
+        //checkout tıklatılıyor
+        MyFunc.Bekle(2);
+        WebElement buttonCheckOut= driver.findElement(By.xpath("//button[@id='checkout']"));
+        buttonCheckOut.click();
+
+        //Kullanıcı bilgileri giriliyor
+        MyFunc.Bekle(2);
+        WebElement firstname= driver.findElement(By.xpath("//input[@id='first-name']"));
+        firstname.sendKeys("Kaan Hakan");
+
+        MyFunc.Bekle(2);
+        WebElement lastname= driver.findElement(By.xpath("//input[@id='last-name']"));
+        lastname.sendKeys("Gül");
+
+        MyFunc.Bekle(2);
+        WebElement zipcode= driver.findElement(By.xpath("//input[@id='postal-code']"));
+        zipcode.sendKeys("34777");
+
+        //continue ya tıklatılıyor
+        MyFunc.Bekle(2);
+        WebElement btnContinue= driver.findElement(By.xpath("//input[@id='continue']"));
+        btnContinue.click();
+
+        //Sepet toplam işlemleri yapılıyor
+        List<WebElement> urunFiyatlari=driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
+
+        double urunlerFiyatToplami=0;
+        for (WebElement e: urunFiyatlari)
+        {
+            System.out.println(e.getText());  // //$15.99
+            String strUrunFiyat=e.getText().replaceAll("[^0-9,.]","");  // rakam ve nokta ve virgül dışındakileri sil
+            double urunFiyat=Double.parseDouble(strUrunFiyat);
+            urunlerFiyatToplami+=urunFiyat;
+        }
+
+        //Alt toplamı bulalım:  item total alınıyor
+        WebElement itemtotalElement= driver.findElement(By.xpath("//div[@class='summary_subtotal_label']"));
+        String strItemTotal = itemtotalElement.getText().replaceAll("[^0-9,.]","");
+        double itemTotal=Double.parseDouble(strItemTotal);
+
+        Assert.assertTrue("Toplam hatalı", urunlerFiyatToplami == itemTotal);
+
+        BekleKapat();
+    }
+
+
 }
