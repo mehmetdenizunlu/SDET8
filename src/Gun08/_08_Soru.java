e1a274e6594efd372641a810025d08d75ed928cc
TechnoStudy <ismettechno@gmail.com>
2025-03-18 17:45:25 +0000

    Soru

---
+++ src/Gun08/_08_Soru.java
@@ -0,0 +1,12 @@
+package Gun08;
+
+public class _08_Soru {
+
+//    https://www.hepsiburada.com.tr/ sayfasına gidiniz.
+//    Moda -> Pantolon  click
+//    tıklatma işleminden sonra URL de
+//    pantolon kelimesinin geçtiğini doğrulayın.
+//    Yukarıdaki aksyonlar için Actions sınıfını kullanınız
+
+
+}
