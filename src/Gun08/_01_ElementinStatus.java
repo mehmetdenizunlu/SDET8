d0a093d417275f18e1bca93a538a53a2f02bf409
TechnoStudy <ismettechno@gmail.com>
2025-03-18 16:28:21 +0000

    Action class

---
+++ src/Gun08/_01_ElementinStatus.java
@@ -0,0 +1,37 @@
+package Gun08;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _01_ElementinStatus extends BaseDriver {
+
+    @Test
+    public void Test1()
+    {
+        driver.get("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCheckBox");
+        MyFunc.Bekle(2);
+
+        WebElement sali=driver.findElement(By.xpath("//input[@id='gwt-debug-cwCheckBox-Tuesday-input']"));
+        // WebElementin durumları: GörünürDurumdaMı,EtkinDurumdaMı,SeçiciliDurumdaMı
+
+        System.out.println("sali.isDisplayed() = " + sali.isDisplayed()); // web de görünüyor mu
+        System.out.println("sali.isEnabled() = " + sali.isEnabled());  // web de etkin mi
+        System.out.println("sali.isSelected() = " + sali.isSelected());  // seçili mi (çekli mi)
+
+        sali.click();  // seçili hale geldi
+        System.out.println("Tıklama sonrası sali.isSelected() = " + sali.isSelected());  // seçili mi (çekli mi)
+
+        /*** cumartesi ***/
+        WebElement cumartesi=driver.findElement(By.xpath("//input[@id='gwt-debug-cwCheckBox-Saturday-input']"));
+        System.out.println("cumartesi.isDisplayed() = " + cumartesi.isDisplayed()); // web de görünüyor mu
+        System.out.println("cumartesi.isEnabled() = " + cumartesi.isEnabled());  // web de etkin mi
+        System.out.println("cumartesi.isSelected() = " + cumartesi.isSelected());  // seçili mi (çekli mi)
+
+        BekleKapat();
+    }
+
+
+}
