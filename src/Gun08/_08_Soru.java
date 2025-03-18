83669d97cdaa2e0a0766b58076f747c6f263f47f
TechnoStudy <ismettechno@gmail.com>
2025-03-18 17:50:13 +0000

    Soru ek

--- src/Gun08/_08_Soru.java
+++ src/Gun08/_08_Soru.java
@@ -1,12 +1,30 @@
 package Gun08;
 
-public class _08_Soru {
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.interactions.Actions;
+
+public class _08_Soru extends BaseDriver {
 
 //    https://www.hepsiburada.com.tr/ sayfasına gidiniz.
 //    Moda -> Pantolon  click
 //    tıklatma işleminden sonra URL de
 //    pantolon kelimesinin geçtiğini doğrulayın.
 //    Yukarıdaki aksyonlar için Actions sınıfını kullanınız
+
+    @Test
+    public void Test1() {
+        driver.get("https://www.hepsiburada.com.tr/");
+        MyFunc.Bekle(2);
+        Actions aksiyonlar = new Actions(driver);
+
+
+
+
+
+        BekleKapat();
+    }
 
 
 }
