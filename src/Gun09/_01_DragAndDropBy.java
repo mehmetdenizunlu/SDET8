9d936db01ccad92f5bf8d453fe7045fb3529e9da
TechnoStudy <ismettechno@gmail.com>
2025-03-19 17:16:57 +0000

    todo

--- src/Gun09/_01_DragAndDropBy.java
+++ src/Gun09/_01_DragAndDropBy.java
@@ -12,20 +12,30 @@ import java.util.List;
 public class _01_DragAndDropBy extends BaseDriver {
 
     @Test
-    public void test1(){
+    public void test1() {
         driver.get("http://talkerscode.com/webtricks/demo/demo_price-range-slider-using-jquery-css-and-php.php");
         MyFunc.Bekle(2);
 
-        List<WebElement> consentButton=driver.findElements(By.xpath("//p[text()='Consent']"));
+        List<WebElement> consentButton = driver.findElements(By.xpath("//p[text()='Consent']"));
         if (consentButton.size() > 0)  // bu element var ise ekranda
             consentButton.get(0).click();
 
-        WebElement solSurgu=driver.findElement(By.xpath("//div[@id='slider-range']/span[1]"));
+        Actions aksiyonDriver = new Actions(driver);
 
-        new Actions(driver).dragAndDropBy(solSurgu, 100, 0).build().perform();
+        WebElement solSurgu = driver.findElement(By.xpath("//div[@id='slider-range']/span[1]"));
+        aksiyonDriver.dragAndDropBy(solSurgu, 50, 0).build().perform();
         // yatay yönde 100 ileri, dikey yönde 0 hareket
+        // + değer girdiğimizde sağa dogru, - değer girdiğimizde sola dogru kaydırıyor
 
-       BekleKapat();
+        WebElement sagSurgu = driver.findElement(By.xpath("//div[@id='slider-range']/span[2]"));
+        aksiyonDriver.dragAndDropBy(sagSurgu, -50, 0).build().perform();
+
+        BekleKapat();
+        // TODO:sol sürgü 200$ , sağ sürgüyü 400$ a getiriniz.
+//        int genislik=cubuk.getSize().width;  500$
+//        (genislik/500)*200  -> sol sürgü için verilecek miktar
+        // isteyen de mediamarket dekileri yapsın
+
     }
 
 }
