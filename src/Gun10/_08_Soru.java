e9b4f436546d5a68518ba60456457dfa1e108127
TechnoStudy <ismettechno@gmail.com>
2025-03-20 18:40:58 +0000

    _08_Soru düzeltme

--- src/Gun10/_08_Soru.java
+++ src/Gun10/_08_Soru.java
@@ -5,7 +5,7 @@ public class _08_Soru {
 //    Senaryo
 //    1- https://testsheepnz.github.io/BasicCalculator.html sitesine gidiniz.
 //    2- random 100 e kadar 2 sayı üretiniz.
-//    3- Sayıları hesap makinesinin bütün fonksiyonları için çalıştırınız.
+//    3- Sayıları hesap makinesinin sadece Add(toplama) için çalıştırınız.
 //    4- Sonuçları Assert ile kontrol ediniz.
 //    5- Yukarıdaki işlemi 5 kez tekrar ettiriniz.
 
